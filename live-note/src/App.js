0ea74b0d1aab520c8a48c8e335ede3da2cf1137a
Vivesh Yadav <viveshofficial@gmail.com>
2020-03-29 23:27:06 +0545

    Add update and delete feature

--- live-note/src/App.js
+++ live-note/src/App.js
@@ -15,7 +15,7 @@ class App extends React.Component{
     super();
     this.state = {
       selectedNoteIndex: null,
-      selectdedNote: null,
+      selectedNote: null,
       notes: null
     };
   }
@@ -30,8 +30,19 @@ class App extends React.Component{
           deleteNote ={this.deleteNote}
           selectNote = {this.selectNote}
           newNote = {this.newNote}
+         
         ></SidebarComponent>
-        <EditorComponent></EditorComponent>
+         {
+            (this.state.selectedNote)? 
+            <EditorComponent 
+              selectedNote = {this.state.selectedNote}
+              selectedNoteIndex={this.state.selectedNoteIndex}
+              noteUpdate = {this.noteUpdate}
+            >
+            </EditorComponent> :
+            null 
+         }
+          
        
       </div>
     );
@@ -50,8 +61,60 @@ class App extends React.Component{
         this.setState({ notes: notes });
       });
   }
-  selectNote = (note , index) =>this.setState({selectedNoteIndex : index , selectdedNote : note});
-
+  selectNote = (note , index) =>this.setState({selectedNoteIndex : index , selectedNote : note});
+  noteUpdate =(id , noteObj) =>{
+    firebase
+      .firestore()
+      .collection('notes')
+      .doc(id)
+      .update(
+        {
+          title :noteObj.title,
+          body : noteObj.body,
+          timestamp : firebase.firestore.FieldValue.serverTimestamp()
+        }
+      )
+  }
+  newNote = async (title) =>{
+    const note = {
+      title : title ,
+      body : ''
+    };
+    const newFromDB  = await firebase 
+      .firestore()
+      .collection('notes')
+      .add({
+        title:note.title,
+        body:note.body,
+        timestamp : firebase.firestore.FieldValue.serverTimestamp()
+       });
+       const newID = newFromDB.id;   
+       await this.setState({
+         notes : [...this.state.notes , note]
+       }) ;
+       const newNoteIndex = this.state.notes.indexOf(this.state.notes.filter(_note =>_note.id ===newID)[0])
+       this.setState({
+         selectedNote: this.state.notes[newNoteIndex],
+         selectedNoteIndex : newNoteIndex
+       });
+    }
+    deleteNote = async (note) => {
+      const noteIndex = this.state.notes.indexOf(note);
+      await this.setState({ notes: this.state.notes.filter(_note => _note !== note) });
+      if(this.state.selectedNoteIndex === noteIndex) {
+        this.setState({ selectedNoteIndex: null, selectedNote: null });
+      } else {
+        this.state.notes.length > 1 ?
+        this.selectNote(this.state.notes[this.state.selectedNoteIndex - 1], this.state.selectedNoteIndex - 1) :
+        this.setState({ selectedNoteIndex: null, selectedNote: null });
+      }
+  
+      firebase
+        .firestore()
+        .collection('notes')
+        .doc(note.id)
+        .delete();
+    }
 }
 
 export default App;
--- live-note/src/editor/editor.js
+++ live-note/src/editor/editor.js
@@ -15,10 +15,36 @@ class EditorComponent extends React.Component {
 
         };
     }
+
+    componentDidMount = () =>{
+        this.setState({
+            text : this.props.selectedNote.body,
+            title : this.props.selectedNote.title,
+            id : this.props.selectedNote.id
+        });
+    }
+    componentDidUpdate = () =>{
+
+        if(this.props.selectedNote.id !== this.state.id){
+            this.setState({
+                text : this.props.selectedNote.body,
+                title : this.props.selectedNote.title,
+                id : this.props.selectedNote.id
+            });
+        }
+    }
     render(){
         const { classes } = this.props;
         return(
          <div className={classes.editorContainer}>
+            <BorderColorIcon className={classes.editIcon}></BorderColorIcon>
+                <input
+                className={classes.titleInput}
+                placeholder='Note title...'
+                value={this.state.title ? this.state.title : ''}
+                onChange={(e) => this.updateTitle(e.target.value)}>
+                </input>
+
             <ReactQuill
              value ={this.state.text}
              onChange={this.updateBody}
@@ -32,8 +58,16 @@ class EditorComponent extends React.Component {
         await this.setState({text : val});
         this.update();
     };
+    updateTitle = async (txt) => {
+        await this.setState({ title: txt });
+        this.update();
+      }
+    
     update = debounce(()=> {
-        
+        this.props.noteUpdate(this.state.id ,{
+            title : this.state.title,
+            body : this.state.text
+        })    
     },1500);
 }
 
